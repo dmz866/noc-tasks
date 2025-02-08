581d62af8302b5e33de9270726d37ac1919c18fc
David Murillo <dmz866@hotmail.com>
2025-02-08 18:05:32 -0500

    logger added

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,13 @@
+import { FileSystemDataSource } from '../infrastructure/data-sources/file-system.data-source';
+import { LogRepository } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
 import { CheckService } from './domain/use-cases/checks/check-service';
 
+const logRepository = new LogRepository(new FileSystemDataSource());
+
 export class Server {
     static start() {
-        const onTick = () => { new CheckService().execute('www.google.com') };
+        const onTick = () => { new CheckService(logRepository).execute('https://www.google.com') };
         CronService.createJob('*/5 * * * * *', onTick);
     }
 }
